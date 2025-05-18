f817f6dac3728e6b482f2ad50adc792f5e4e5727
Fredrik von von van Helleblad <115473122+Fredde420@users.noreply.github.com>
2025-05-19 01:09:21 +0200

    Gjort Start user och staff login, lite på MinaS
    Gjort Start, user och staff login, lite på MinaSidor(Dashboard user staff)

---
+++ src/main/java/com/example/javafx/UserDashboardController.java
@@ -0,0 +1,21 @@
+package com.example.javafx;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.Node;
+import java.io.IOException;
+
+public class UserDashboardController {
+
+    @FXML
+    private void logout(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Start.fxml"));
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+}
